12164e4accbc76edfa50ba349ecea6d5dac243d9
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 12:46:25 +0200

    testing still

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import './App.css'
-// import MainStuff from './Mainstuff'
+import MainStuff from './Mainstuff'
 import Footer from './Footer'
 
-const MainStuff: React.FC = () => {
-  return (
-    <div className="group flex flex-col items-center justify-center mx-auto mt-4 p-1.5 sm:w-[30%] sm:h-[40%] w-[80%] h-[90%] bg-white border-2 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:rotate-2">
-      <img className="rounded-lg hover:-rotate-3 transition-transform duration-500 ease-in-out " src="../images/image-qr-code.png" alt="QR Code" />
-      <div className="text-center mt-4">
-        <strong className="font-bold text-xl">Improve your front-end skills by building projects</strong>
-        <p className="text-gray-700">
-          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-        </p>
-      </div>
-    </div>
-  );
-};
 
 function App() {
 
---
+++ src/Mainstuff.tsx
@@ -0,0 +1,17 @@
+const Mainstuff = () => {
+    return (
+        <div className="group flex flex-col items-center justify-center mx-auto mt-4 p-1.5 sm:w-[30%] sm:h-[40%] w-[80%] h-[90%] bg-white border-2 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:rotate-2">
+            <div className="rounded-lg hover:-rotate-3 transition-transform duration-500 ease-in-out ">
+                <img src="./images/image-qr-code.png" alt="QR Code" />
+            </div>
+            <div className="text-center mt-4">
+                <strong className="font-bold text-xl">Improve your front-end skills by building projects</strong>
+                <p className="text-gray-700">
+                    Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Mainstuff
