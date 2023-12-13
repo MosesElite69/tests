9aeda18d06f3381fd87e1173e59e9556df21edd0
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 12:29:33 +0200

    testing still

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainStuff from './Mainstuff'
+import Footer from './Footer'
 function App() {
 
   return (
     <>
       <MainStuff />
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer className="text-[11px] text-center mt-6">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a className="underline bold text-3" href="https://www.frontendmentor.io/profile/MosesElite69">MosesElite69</a>.
+        </footer>
+    )
+}
+
+export default Footer
