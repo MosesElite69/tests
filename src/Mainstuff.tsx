7a139b86ee7fd5d0df9edbd6af4e380035d18d04
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 13:01:31 +0200

    testing still

--- src/Mainstuff.tsx
+++ src/Mainstuff.tsx
@@ -2,7 +2,7 @@ const Mainstuff = () => {
     return (
         <div className="group flex flex-col items-center justify-center mx-auto mt-4 p-1.5 sm:w-[30%] sm:h-[40%] w-[80%] h-[90%] bg-white border-2 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:rotate-2">
             <div className="rounded-lg hover:-rotate-3 transition-transform duration-500 ease-in-out ">
-                <img src="../public/images/image-qr-code2.png" alt="QR Code" />
+                <img src="../public/images/high-angle-desk-assortment-with-laptop.jpg" />
             </div>
             <div className="text-center mt-4">
                 <strong className="font-bold text-xl">Improve your front-end skills by building projects</strong>
