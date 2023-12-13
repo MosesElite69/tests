807c4359b553ea8f5200441807b8a3cc8b27faae
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 14:40:40 +0200

    finished

--- src/Mainstuff.tsx
+++ src/Mainstuff.tsx
@@ -1,7 +1,9 @@
+import QrCode from "../public/images/image-qr-code.png"
+
 const Mainstuff = () => {
     return (
         <div className="group flex flex-col items-center justify-center mx-auto mt-4 p-1.5 sm:w-[30%] sm:h-[40%] w-[80%] h-[90%] bg-white border-2 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:rotate-2">
-            <img className="rounded-lg hover:-rotate-3 transition-transform duration-500 ease-in-out " src="../public/images/image-qr-code.png" />
+            <img className="rounded-lg hover:-rotate-3 transition-transform duration-500 ease-in-out " src={QrCode} />
             <div className="text-center mt-4">
                 <strong className="font-bold text-xl">Improve your front-end skills by building projects</strong>
                 <p className="text-gray-700">
